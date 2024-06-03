ce0da4ffcce734f818bde63a0781fab8b7732fb0
artёm rudoy <art.rudy@gmail.com>
2024-06-03 18:56:17 +0300

    entity class

---
+++ API/Entities/Product.cs
@@ -0,0 +1,10 @@
+namespace API.Entities
+{
+    public class Product
+    {
+        public int MyProperty { get; set; }
+
+        public string Name { get; set; }
+        
+    }
+}
--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0fd57a9dbc1d48a737767b3b54f855e171381e87")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d32e34b72817c9740b3d64ea0f927e14362da6f")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
